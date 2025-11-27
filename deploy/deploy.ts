b851350b1313d32cf07a70a674380869389fdf8e
maria-fern-build <qhyqwx78@outlook.com>
2025-11-27 22:12:51 +0800

    update deploy ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const primeLaunchFactory = await deploy("PrimeLaunchFactory", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`PrimeLaunchFactory contract:`, primeLaunchFactory.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_primeLaunchFactory"; // id required to prevent reexecution
+func.tags = ["PrimeLaunchFactory"];
